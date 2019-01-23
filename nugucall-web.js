023e835e8cebbcdb5df5f3093092dccee1622333
ldokyung98 <eehrud@gmail.com>
2019-01-23 11:30:24 +0900

    190123 IP 및 포트 수정

--- nugucall-web.js
+++ nugucall-web.js
@@ -10,8 +10,8 @@ var SocketServer = require('net');
 var WebSocketServer = require('websocket').server;
 
 // 포트 정의
-var SOCKET_UP_PORT = 8081;
-var SOCKET_DOWN_PORT = 8082;
+var SOCKET_UP_PORT = 8083;
+var SOCKET_DOWN_PORT = 8084;
 
 var WEB_SERVER_PORT = 8085;
 
